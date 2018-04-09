2db4adcdaf8df6323b459b4b2cbfd39b62670c4f
Marcel Anis <marcel.anis.21@gmail.com>
2018-04-09 13:16:05 -0400

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID:'964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://manis21:Rollingthunder1@ds117759.mlab.com:17759/blog_dev_1',
-  cookieKey: '123123123'
+  googleClientID:'',
+  googleClientSecret: '',
+  mongoURI: '',
+  cookieKey: ''
 };
